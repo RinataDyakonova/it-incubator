83e3b913abe52197b17d58b3790afd687b211bc2
Rinata21 <djakonovarinata@gmail.com>
2023-06-19 22:08:07 +0500

    Students Final Grade

--- codewars/08.js
+++ codewars/08.js
@@ -1,11 +1,6 @@
-function finalGrade(e, p) {
-  if (e > 90 || p > 10) {
-    return 100;
-  } else if (e > 75 && p >= 5) {
-    return 90;
-  } else if (e > 50 && p) {
-    return 75;
-  } else {
-    return 0;
-  }
-}
+function finalGrade (exam, projects) {
+ if(exam > 90 || projects > 10) return 100;
+ if(exam > 75 & projects >= 5) return 90;
+ if(exam > 50 & projects >= 2) return 75;
+   return 0;
+ }
